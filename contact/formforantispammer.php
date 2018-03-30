b82d962301569157367ddc14a2487f68e3287211
Timmy <Timmy@Timothys-MacBook-Pro.local>
2018-03-30 17:56:09 -0400

    fix php header bug

--- contact/formforantispammer.php
+++ contact/formforantispammer.php
@@ -5,20 +5,16 @@ $field_number = $_POST['number'];
 $field_message = $_POST['message'];
 
 $to = 'esther@beargarden4kids.com';
-$subject = 'Message from a site visitor '.$field_name;
+$subject = 'Message from a site visitor:'.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
 $body_message .= 'E-mail: '.$field_email."\n";
 $body_message .= 'Phone number: '.$field_number."\n";
 $body_message .= 'Message: '.$field_message;
 
-$headers .= "\r\nReply-To: $field_email";
+$headers = "Reply-To: $field_email";
 
 if(isset($_POST['url']) && $_POST['url'] == ''){
-
-	if(!($_POST['email'] && !preg_match( "/[\r\n]/", $_POST['email'])) ) {
-	  $headers = "From: $youremail";
-	}
 
 	$mail_status = mail($to, $subject, $body_message, $headers);
 } ?>
